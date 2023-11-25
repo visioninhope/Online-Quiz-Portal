b4d148480f6e9425b9e81766d61f24832a69ef83
BekCodingAddict <experte001@gmail.com>
2023-11-25 17:11:32 +0900

    Login Screen has been designed!

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import { Button, Flex } from 'antd';
+//import { Button, Flex } from 'antd';
 import "./stylesheets/theme.css"
 import "./stylesheets/alignments.css"
 import "./stylesheets/textelements.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/common/Register';
+import Login from './pages/common/Login';
+import './stylesheets/custom-components.css';
+import './stylesheets/form-elements.css';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <div className="h-screen bg-primary">
-
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
