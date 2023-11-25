f86bc013dbcee9f200f39446b2c50d2f366f5b9e
BekCodingAddict <experte001@gmail.com>
2023-11-25 15:25:26 +0900

    The ReactApp Setup has been finished and Started Ant Introduction

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Button, Flex } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Online Quiz Portal</h1>
+      <Button
+      type='primary'>
+        Primary Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
