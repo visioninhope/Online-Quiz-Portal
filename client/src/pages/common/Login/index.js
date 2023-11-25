73f590e6c4923bf7f4dc4637890f2cdfdc1dc0d1
BekCodingAddict <experte001@gmail.com>
2023-11-25 17:38:16 +0900

    Registration screen has been designed and Authentication UI has been finished

--- client/src/pages/common/Login/index.js
+++ client/src/pages/common/Login/index.js
@@ -1,22 +1,29 @@
 import React from "react";
-import {Form} from "antd";
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
-function Login(){
+function Login() {
+    const onFinish=(values)=>{
+        console.log(values)
+    }
     return (
         <div className="flex justify-center item-center h-screen w-screen">
             <div className="card w-400 p-3">
                 <div className="flex flex-col">
                     <h1 className="text-2xl">Login</h1>
                     <div className="divider"></div>
-                    <Form layout="vertical">
+                    <Form layout="vertical" className="mt-2" onFinish={onFinish}>
                         <Form.Item name="email" label="Email">
-                            <input type="text"/>
+                            <input type="text" />
                         </Form.Item>
                         <Form.Item name="password" label="Password">
-                            <input type="password"/>
+                            <input type="password" />
                         </Form.Item>
-                        
-                        <button type="submit" className="primary-contained-btn mt-2 w-100">Login</button>
+
+                        <div className="flex flex-col gap-2">
+                            <button type="submit" className="primary-contained-btn mt-2 w-100">Login</button>
+                            <Link to='/register' className="underline">Not a member? Register</Link>
+                        </div>
                     </Form>
                 </div>
             </div>
--- client/src/pages/common/Register/index.js
+++ client/src/pages/common/Register/index.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
-function Register(){
+function Register() {
+    const onFinish=(values)=>{
+        console.log(values)
+    }
     return (
-        <div>
-            Register
+        <div className="flex justify-center item-center h-screen w-screen">
+            <div className="card w-400 p-3">
+                <div className="flex flex-col">
+                    <h1 className="text-2xl">Register</h1>
+                    <div className="divider"></div>
+                    <Form layout="vertical" className="mt-2" onFinish={onFinish}>
+                    <Form.Item name="name" label="Name">
+                            <input type="text" />
+                        </Form.Item>
+                        <Form.Item name="email" label="Email">
+                            <input type="text" />
+                        </Form.Item>
+                        <Form.Item name="password" label="Password">
+                            <input type="password" />
+                        </Form.Item>
+
+                        <div className="flex flex-col gap-2">
+                            <button type="submit" className="primary-contained-btn mt-2 w-100">Login</button>
+                            <Link to='/login' className="underline">Already a member? Login</Link>
+                        </div>
+                    </Form>
+                </div>
+            </div>
         </div>
     )
 }
