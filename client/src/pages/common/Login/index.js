d809fdda1eb9871f7c5dba5e79fc917e82ed4367
BekCodingAddict <experte001@gmail.com>
2023-12-01 23:21:08 +0900

    Login API has been created and BackEnd Authentication has been finished!

--- client/src/pages/common/Login/index.js
+++ client/src/pages/common/Login/index.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Form } from "antd";
+import { Form, message } from "antd";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../../apicalls/users";
 
 function Login() {
-    const onFinish=(values)=>{
-        console.log(values)
+    const onFinish = async (values) => {
+        try {
+          const response = await loginUser(values);
+          if (response.success) {
+            message.success(response.message);
+            localStorage.setItem("token", response.data);
+          } else {
+            message.error(response.message);
+          }
+        } catch (error) {
+          message.error(error.message);
+        }
     }
+
     return (
         <div className="flex justify-center item-center h-screen w-screen">
             <div className="card w-400 p-3">
--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //user-registration 
 router.post('/register', async (req, res) => {
@@ -34,5 +35,44 @@ router.post('/register', async (req, res) => {
     }
 });
 
-module.exports=router;
+//user login
+router.post("/login", async (req, res) => {
+    try {
+      // check if user exists
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return res
+          .status(200)
+          .send({ message: "User does not exist", success: false });
+      }
+  
+      // check password
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!validPassword) {
+        return res
+          .status(200)
+          .send({ message: "Invalid password", success: false });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+  
+      res.send({
+        message: "User logged in successfully",
+        success: true,
+        data: token,
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: error.message,
+        data: error,
+        success: false,
+      });
+    }
+  });
+module.exports = router;
 
